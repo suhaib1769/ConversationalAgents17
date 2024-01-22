df6f285c19b4cf66574c06d55a36415ad85921b1
Amar Mesic <amar.s.mesic@gmail.com>
2024-01-22 13:45:27 +0100

    add question answering interface

--- furhat_test.py
+++ furhat_test.py
@@ -1,5 +1,22 @@
 from furhat_remote_api import FurhatRemoteAPI
+from questionModule import preprocess_question
 from triples.triple import extract_triples1, triple_embedding, metadata_embedding
+
+
+### helper functions
+# send the query to GPT API
+def ask_GPT(query):
+    pass
+
+# have furhat respond to the query
+def respond(response):
+    furhat.say(response)
+
+# create an episode from user memory
+def add_to_memory(user_input):
+    pass
+
+
 
 # Create an instance of the FurhatRemoteAPI class, providing the address of the robot or the SDK running the virtual robot
 furhat = FurhatRemoteAPI("localhost")
@@ -28,6 +45,7 @@ while True:
 
 
     # Check if user said "bye" to break the loop
+    # TODO - make sure message ends with "bye"
     if result.message.lower() == "bye":
         furhat.say(text="Goodbye!")
         break
@@ -57,8 +75,22 @@ while True:
         ],
         "class": "furhatos.gestures.Gesture"
     })
-    
-    # furhat.say(result.message)
+
+    ### CONVERSATIONAL PIPELINE HERE
+    user_input = result.message
+    intent = None
+    response = None
+
+    # check if the utterance is a question or statement
+    if '?' in user_input:
+        if intent == 'oos':
+            intent_string = intent.replace('_', ' ')
+            respond("Sorry, as a restaurant booking agent I cannot answer specific questions about {}.".format(intent_string))
+        else:
+            ask_GPT(user_input)
+    else:
+        add_to_memory(user_input)
+        # TODO - personalized response pipeline
 
     # Perform a named gesture
     furhat.gesture(name="BrowRaise")
@@ -71,6 +103,7 @@ while True:
 
     # Set the LED lights
     furhat.set_led(red=200, green=50, blue=50)
+
 
 # Set the LED lights
 furhat.set_led(red=0, green=0, blue=0)
